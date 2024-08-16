21442565909809c04e744ad123be77cd4d728bfe
Givanna Putri <ghar1821@users.noreply.github.com>
2024-08-16 11:27:25 +1000

    update vignettes

--- doc/TrackSOM-workflow.R
+++ doc/TrackSOM-workflow.R
@@ -1,4 +1,5 @@
 ## ----message=FALSE, warning=FALSE---------------------------------------------
+library(data.table)
 library(TrackSOM)
 
 ## -----------------------------------------------------------------------------
@@ -26,18 +27,22 @@ data.files.fullpath.fcs <- c(
 print(data.files.fullpath.fcs)
 
 ## -----------------------------------------------------------------------------
-library(data.table)
-data.files <- c(
-  system.file("extdata", "synthetic_d0.csv", package = "TrackSOM"),
-  system.file("extdata", "synthetic_d1.csv", package = "TrackSOM"),
-  system.file("extdata", "synthetic_d2.csv", package = "TrackSOM"),
-  system.file("extdata", "synthetic_d3.csv", package = "TrackSOM"),
-  system.file("extdata", "synthetic_d4.csv", package = "TrackSOM")
-)
-dat <- lapply(data.files, function(f) fread(f))
+dat <- lapply(data.files.fullpath, function(f) fread(f))
+dat
 
 ## -----------------------------------------------------------------------------
-dat
+timepoints <- seq(0, 4)
+
+dat <- lapply(seq(length(data.files.fullpath)), function(data_file_i) {
+    dt <- fread(data.files.fullpath[[data_file_i]])
+    dt[['timepoint']] <- timepoints[data_file_i]
+    return(dt)
+})
+
+dat <- rbindlist(dat)
+
+head(dat)
+tail(dat)
 
 ## -----------------------------------------------------------------------------
 tracksom.result <- TrackSOM(inputFiles = data.files.fullpath,
@@ -49,7 +54,6 @@ tracksom.result <- TrackSOM(inputFiles = data.files.fullpath,
 )
 
 ## -----------------------------------------------------------------------------
-library(data.table)
 data.files <- c(
   system.file("extdata", "synthetic_d0.csv", package = "TrackSOM"),
   system.file("extdata", "synthetic_d1.csv", package = "TrackSOM"),
